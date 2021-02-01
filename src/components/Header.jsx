f18b14563d37e836feb88f8011c495683061f329
torco34 <torcoromaarias@gimail.com>
2021-02-01 10:26:32 -0500

    Elheader de html

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
-import '../assets/styles/App.scss';
-
+// import '../assets/styles/App.scss';
 const Header = () => ( 
-	<h1> Hola Mundo </h1>
+	<header id="foto_header" className=" container header">
+		<nav id="header" className="nav">
+			<ul className="active" id="">
+				<li className="active" id="inicio" data-body="#taps1">Inicio</li>
+				<li id="menu" data-body="#taps2">Pizza</li>
+				<li id="sabor" data-body="#taps3">Adicionales</li>
+				<li id="ingresar" data-body="#taps4">Bebidas</li>
+			</ul>
+		</nav>
+	</header>
 );
 
 export default Header
