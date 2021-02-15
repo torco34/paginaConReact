7d3cffd955303b5e958ebca59b724d5fc7b9d231
torco34 <torcoromaarias@gimail.com>
2021-02-15 00:48:41 -0500

    Parrafos de informacion

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,22 +3,27 @@ import React, { Component } from "react";
 class Header extends Component {
 	render() {
 		return (
-			<div className="card">
+			<div className="card bg-secondary">
 				<div className="row ">
 					<div className="text-center col-md-9 ">
 						<h5 className="m-3">
 							Tus{" "}
-							<mark className="bg-danger text-white">
+							<mark className="btn text-white btn-primary">
 								cambio o devulucion en tu App:
 							</mark>{" "}
 							y tambien dede tu movil o cumputador..
 						</h5>
 					</div>
 					<div className="col-md-3 ">
-						<button className=" btn  btn-danger m-3">
+						<button className=" btn  btn-primary m-3">
 							conocer mas
 						</button>
 					</div>
+					<p className="parrafo"> Servicios Especiales de Entrega</p>
+					<p> 90 Días de cambios y/o devoluciones</p>
+					<p> Sigue tu pedido</p>
+					<p> Contáctanos</p>
+					<p> ConócenosServicio al Cliente</p>
 				</div>
 			</div>
 		);
